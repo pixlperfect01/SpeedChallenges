e14d65643f41a452f8ad652ef8f1f6da39a5941c
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-04 20:43:15 -0600

    Update main.js

--- SpeedChallenges/chellenge-1/main.js
+++ SpeedChallenges/chellenge-1/main.js
@@ -1,1 +1,77 @@
+function player(){
+  this.x=0;
+  this.y=0;
+  this.up=function(){
+    if(this.y>0){
+      this.y--;
+    }
+  }
+  this.down=function(){
+    if(this.y<c.height){
+      this.y++;
+    }
+  }
+  this.right=function(){
+    if(this.x<c.width){
+      this.x++;
+    }
+  }
+  this.left=function(){
+    if(this.x>0){
+      this.x--;
+    }
+  }
+}
+
 var c,ctx;
+setTimeout(setup,500);
+function setup(){
+  c=document.getElementById("c");
+  ctx=c.getContext("2d");
+  draw();
+}
+
+function draw(){
+  if(keys.up)
+    p.up();
+  if(keys.down)
+    p.up();
+  if(keys.left)
+    p.up();
+  if(keys.right)
+    p.up();
+  p.draw();
+}
+
+
+
+
+
+var keys = { 
+up: false, 
+down: false, 
+left: false, 
+right: false 
+} 
+function keyUpdate(keyEvent, down) { 
+console.log(keyEvent.keyCode);
+switch (keyEvent.keyCode) { 
+case 38: keys.up = down; 
+break; 
+case 40: 
+keys.down = down; 
+break; 
+case 37: 
+keys.left = down; 
+break; 
+case 39: 
+keys.right = down; 
+break; 
+} 
+} 
+document.addEventListener("keydown", function(event) { 
+keyUpdate(event, true); 
+}); 
+document.addEventListener("keyup", function(event) { 
+keyUpdate(event, false); 
+});
