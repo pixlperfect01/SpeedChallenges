016c45da79d899ff691b134155e7359dc6e564aa
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-07 09:37:24 -0600

    Update and rename SpeedChallenges/challenge-1/main.js to SpeedChallenges/physicsSimulator/main.js

--- SpeedChallenges/physicsSimulator/main.js
+++ SpeedChallenges/physicsSimulator/main.js
@@ -1,8 +1,44 @@
 function player(x, y){
+  this.loc=createVector(x, y);
+  this.vel=createVector(0, 0);
+  this.acc=createVector(0, 0);
+  this.jump=function(){
+    this.vel
+  }
+}
+function createVector(x_, y_){
+  return new Vector(x_, y_);
+}
+function vector(x, y){
   this.x=x;
   this.y=y;
+  this.add=function(a, b){
+    if(!b){
+      this.x+=a.x;
+      this.y+=a.y;
+    }else{
+      this.x+=a;
+      this.y+=b;
+    }
+  }
+  this.subtract=function(a, b){
+    if(!b){
+      this.x-=a.x;
+      this.y-=a.y;
+    }else{
+      this.x-=a;
+      this.y-=b;
+    }
+  }
+  this.mult=function(a){
+    this.x*=a;
+    this.y*=a;
+  }
+  this.div=function(a){
+    this.x/=a;
+    this.y/=a;
+  }
 }
-
 setTimeout(setup,500);
 var c,ctx,p;
 function setup(){
